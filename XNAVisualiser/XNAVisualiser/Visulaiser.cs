a283bb7c2f5c5e7904aaff22a676b9d2e94a946c
grimm004 <grimm004@gmail.com>
2017-08-01 17:41:18 +0100

    Added more binds

--- XNAVisualiser/XNAVisualiser/Visulaiser.cs
+++ XNAVisualiser/XNAVisualiser/Visulaiser.cs
@@ -19,19 +19,27 @@ namespace XNAVisualiser
         private Texture2D currentDeck;
         private Texture2D pointTexture;
         private const int BORDER = 25;
-        private const int POINTSIZE = 2;
         private const double MRATE = .01;
         private const float ORATE = .5f;
+        private int updateCounter = 0;
+        private int pointSize = 2;
         private DatabaseManager.Database database;
-        private Vector2[] locations;
+        private List<Vector2> locations;
+        private Vector2 currentPoint;
+        private int currentPointIndex = 0;
         private double multiplier = 7;
+        private bool cyclePoints = false;
         private Vector2 offset = new Vector2(BORDER);
-        private Keybind incrBind;
-        private Keybind decrBind;
+        private Keybind upBind;
+        private Keybind downBind;
+        private Keybind leftBind;
+        private Keybind rightBind;
         private Keybind wBind;
         private Keybind aBind;
         private Keybind sBind;
         private Keybind dBind;
+        private Keybind tBind;
+        private Keybind yBind;
         private Keybind[] binds;
 
         public Visulaiser()
@@ -39,12 +47,29 @@ namespace XNAVisualiser
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             database = new DatabaseManager.CSVDatabase("C:\\Data", false, ".csv");
+            locations = new List<Vector2>();
+        }
+
+        protected void UpdateMac()
+        {
+            Console.WriteLine("Enter new mac address:");
+            string newMac = Console.ReadLine();
             DatabaseManager.Table table = database.GetTable("TUI_D1_location_data_03-12-2017");
-            DatabaseManager.Record[] records3 = table.GetRecords("deck", "Deck3");
-            DatabaseManager.Record[] records4 = table.GetRecords("deck", "Deck4");
-            Console.WriteLine(records4.Length);
-            locations = new Vector2[records4.Length];
-            for (int i = 0; i < locations.Length; i++) locations[i] = new Vector2((float)(double)records4[i].GetValue("x"), (float)(double)records4[i].GetValue("y"));
+            DatabaseManager.Record[] records = table.GetRecords("deck", "Deck4");
+            Console.WriteLine(records.Length);
+            locations = new List<Vector2>();
+            for (int i = 0; i < records.Length; i++)
+                if ((string)records[i].GetValue("mac") == newMac)
+                    locations.Add(new Vector2((float)(double)records[i].GetValue("x"), (float)(double)records[i].GetValue("y")));
+        }
+
+        protected void RenderPointTexture(int size)
+        {
+            if (size < 0) return;
+            pointTexture = new Texture2D(GraphicsDevice, size, size);
+            Color[] data = new Color[size * size];
+            for (int i = 0; i < data.Length; i++) data[i] = Color.Red;
+            pointTexture.SetData(data);
         }
 
         /// <summary>
@@ -56,13 +81,17 @@ namespace XNAVisualiser
         protected override void Initialize()
         {
             IsMouseVisible = true;
-            incrBind = new Keybind(Keys.Up, () => { multiplier += MRATE; }, false);
-            decrBind = new Keybind(Keys.Down, () => { multiplier -= MRATE; }, false);
-            wBind = new Keybind(Keys.W, () => { offset.Y -= ORATE; }, false);
-            aBind = new Keybind(Keys.A, () => { offset.X -= ORATE; }, false);
-            sBind = new Keybind(Keys.S, () => { offset.Y += ORATE; }, false);
-            dBind = new Keybind(Keys.D, () => { offset.X += ORATE; }, false);
-            binds = new Keybind[] { incrBind, decrBind, wBind, aBind, sBind, dBind };
+            upBind = new Keybind(Keys.Up, () => multiplier += MRATE, false);
+            downBind = new Keybind(Keys.Down, () => multiplier -= MRATE, false);
+            leftBind = new Keybind(Keys.Left, () => RenderPointTexture(--pointSize), true);
+            rightBind = new Keybind(Keys.Right, () => RenderPointTexture(++pointSize), true);
+            wBind = new Keybind(Keys.W, () => offset.Y -= ORATE, false);
+            aBind = new Keybind(Keys.A, () => offset.X -= ORATE, false);
+            sBind = new Keybind(Keys.S, () => offset.Y += ORATE, false);
+            dBind = new Keybind(Keys.D, () => offset.X += ORATE, false);
+            tBind = new Keybind(Keys.T, UpdateMac, true);
+            yBind = new Keybind(Keys.Y, () => cyclePoints ^= true, true);
+            binds = new Keybind[] { upBind, downBind, leftBind, rightBind, wBind, aBind, sBind, dBind, tBind, yBind };
             base.Initialize();
         }
 
@@ -73,10 +102,7 @@ namespace XNAVisualiser
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            pointTexture = new Texture2D(GraphicsDevice, POINTSIZE, POINTSIZE);
-            Color[] data = new Color[POINTSIZE * POINTSIZE];
-            for (int i = 0; i < data.Length; i++) data[i] = Color.Red;
-            pointTexture.SetData(data);
+            RenderPointTexture(pointSize);
             currentDeck = Content.Load<Texture2D>("Deck4");
             graphics.PreferredBackBufferWidth = currentDeck.Bounds.Width + (2 * BORDER);
             graphics.PreferredBackBufferHeight = currentDeck.Bounds.Height + (2 * BORDER);
@@ -101,6 +127,14 @@ namespace XNAVisualiser
         {
             if (Keyboard.GetState().IsKeyDown(Keys.Escape)) this.Exit();
             foreach (Keybind bind in binds) bind.Update();
+            
+            if (++updateCounter % 30 == 0)
+            {
+                if (currentPointIndex == locations.Count) currentPointIndex = 0;
+                else currentPoint = locations[currentPointIndex++];
+                updateCounter = 0;
+            }
+
             base.Update(gameTime);
         }
 
@@ -113,7 +147,8 @@ namespace XNAVisualiser
             GraphicsDevice.Clear(Color.White);
             spriteBatch.Begin();
             spriteBatch.Draw(currentDeck, new Vector2(BORDER), Color.White);
-            foreach (Vector2 point in locations) spriteBatch.Draw(pointTexture, offset + ((float)multiplier * point), Color.White);
+            if (!cyclePoints) foreach (Vector2 point in locations) spriteBatch.Draw(pointTexture, offset + ((float)multiplier * point), Color.White);
+            else spriteBatch.Draw(pointTexture, offset + ((float)multiplier * currentPoint), Color.White);
             spriteBatch.End();
             base.Draw(gameTime);
         }
