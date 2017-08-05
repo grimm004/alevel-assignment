1f0578e2fb6150dd966755c2b1a3d087c44b9a4a
grimm004 <grimm004@gmail.com>
2017-08-05 19:27:18 +0100

    Test2

--- XNAVisualiser/XNAVisualiser/Visulaiser.cs
+++ XNAVisualiser/XNAVisualiser/Visulaiser.cs
@@ -48,7 +48,6 @@ namespace XNAVisualiser
             Content.RootDirectory = "Content";
             database = new DatabaseManager.CSVDatabase("C:\\Data", false, ".csv");
             locations = new List<Vector2>();
-            // Test Change
         }
 
         protected void UpdateMac()
