399f12ea7dc0ee1c3a26b44e9b0560d349296bb3
grimm004 <grimm004@gmail.com>
2018-02-23 21:31:12 +0000

    .

---
+++ TestPlugin/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnalysisSDK;
+using DatabaseManagerLibrary;
+
+namespace TestPlugin
+{
+    public class TestPlugin : IAnalysis
+    {
+        public string Description { get { return "A test plugin..."; } }
+
+        public AnalysisResult FetchResult(string metadata)
+        {
+            return new AnalysisResult { Content = "My loaded in analysis results...", Outcome = ResultRequestOutcome.OK };
+        }
+
+        public void Run(Table[] tables, Action<double> percentageCompletionChange)
+        {
+            // Run the analysis with the provided tables here...
+            // Callback the percentageCompletionChange(double) action to show the user what stage the analysis is at...
+        }
+    }
+}
